d716288542cd4e0f18d50a7b068f920fa9f01468
o000o <alberto.ribeiro@aluno.ufca.edu.br>
2022-01-27 04:31:35 -0300

    MySQL settings

--- phpMyAdmin.sh
+++ phpMyAdmin.sh
@@ -3,10 +3,10 @@ sudo apt update
 sudo apt install wget -y
 sudo apt install unzip -y
 sudo apt install apache2 -y
-sudo apt install php libapache2-mod-php php-mysql -y
+sudo apt install php libapache2-mod-php php-mysql php-mbstring -y
 echo "<?php phpinfo(); ?>" >> /var/www/html/phpinfo.php
 sudo apt install mariadb-server mariadb-client -y
-echo -e "# Fix: AH00076: Failed to enable APR_TCP_DEFER_ACCEPT\nAcceptFilter http none\nAcceptFilter https none\n" >> /etc/apache2/apache2.conf
+# echo -e "# Fix: AH00076: Failed to enable APR_TCP_DEFER_ACCEPT\nAcceptFilter http none\nAcceptFilter https none\n" >> /etc/apache2/apache2.conf
 wget -P /var/www/html "https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip"
 unzip -d /var/www/html/phpmyadmin /var/www/html/phpMyAdmin-latest-all-languages.zip
 rm /var/www/html/phpMyAdmin-latest-all-languages.zip
@@ -16,4 +16,6 @@ echo $folder
 mv -f /var/www/html/phpmyadmin/$folder/* /var/www/html/phpmyadmin
 rm -R /var/www/html/phpmyadmin/$folder
 mv config.sample.inc.php config.inc.php
+# sed -i "s/\$cfg\['blowfish_secret'\] = ''\;/\$cfg\['blowfish_secret'\] = '$secret_password'\;/" config.inc.php
+sudo mysql_secure_installation
 sudo service apache2 restart
