665422cd8d6be0194a23e56040247b275f9095cb
Alberto <94637799+meaar@users.noreply.github.com>
2022-04-10 13:55:43 -0300

    Download phpmyadmin in shell script folder

--- phpMyAdmin.sh
+++ phpMyAdmin.sh
@@ -4,18 +4,21 @@ sudo apt install wget -y
 sudo apt install unzip -y
 sudo apt install apache2 -y
 sudo apt install php libapache2-mod-php php-mysql php-mbstring php-xml -y
-sudo echo "<?php phpinfo(); ?>" >> /var/www/html/phpinfo.php
+echo "<?php phpinfo(); ?>" >> phpinfo.php
+sudo mv phpinfo.php /var/www/html
 sudo apt install mariadb-server mariadb-client -y
 # sudo echo -e "# Fix: AH00076: Failed to enable APR_TCP_DEFER_ACCEPT\nAcceptFilter http none\nAcceptFilter https none\n" >> /etc/apache2/apache2.conf
-sudo wget -P /var/www/html "https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip"
-sudo unzip -d /var/www/html/phpmyadmin /var/www/html/phpMyAdmin-latest-all-languages.zip
-sudo rm /var/www/html/phpMyAdmin-latest-all-languages.zip
-cd /var/www/html/phpmyadmin
+wget "https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip"
+unzip -d phpmyadmin phpMyAdmin-latest-all-languages.zip
+directory=$(pwd)
+cd phpmyadmin
 folder=$(ls)
 echo $folder
-sudo mv -f /var/www/html/phpmyadmin/$folder/* /var/www/html/phpmyadmin
-sudo rm -R /var/www/html/phpmyadmin/$folder
-sudo mv config.sample.inc.php config.inc.php
+mv $folder/config.sample.inc.php $folder/config.inc.php
+sudo mkdir /var/www/html/phpmyadmin
+sudo mv -f $folder/* /var/www/html/phpmyadmin
+cd $directory
+rm -R phpmyadmin
 # secret_password="wDdAIJ*@#AU@#(J#fh@(D)_@(#@(JFPK"
 # sudo sed -i "s/\$cfg\['blowfish_secret'\] = ''\;/\$cfg\['blowfish_secret'\] = '$secret_password'\;/" config.inc.php
 sudo service mysql stop
