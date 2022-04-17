2b0cce929e09ed769108ffac347971d990eeae8a
Alberto <94637799+meaar@users.noreply.github.com>
2022-04-17 14:01:41 -0300

    adjusted priorities and added comments

--- phpMyAdmin.sh
+++ phpMyAdmin.sh
@@ -7,7 +7,19 @@ sudo apt install php libapache2-mod-php php-mysql php-mbstring php-xml -y
 echo "<?php phpinfo(); ?>" >> phpinfo.php
 sudo mv phpinfo.php /var/www/html
 sudo apt install mariadb-server mariadb-client -y
-# sudo echo -e "# Fix: AH00076: Failed to enable APR_TCP_DEFER_ACCEPT\nAcceptFilter http none\nAcceptFilter https none\n" >> /etc/apache2/apache2.conf
+sudo service mysql stop
+sudo service mysql start
+sudo mysql_secure_installation
+sudo mysql -u root -p
+############# use root user #############
+# ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+# FLUSH PRIVILEGES;
+#########################################
+############ create new user ############
+# CREATE USER 'new_user'@'localhost' IDENTIFIED BY 'password';
+# GRANT ALL PRIVILEGES ON * . * TO 'new_user'@'localhost';
+# FLUSH PRIVILEGES;
+#########################################
 wget "https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip"
 unzip -d phpmyadmin phpMyAdmin-latest-all-languages.zip
 rm phpMyAdmin-latest-all-languages.zip
@@ -20,14 +32,13 @@ sudo mkdir /var/www/html/phpmyadmin
 sudo mv -f $folder/* /var/www/html/phpmyadmin
 cd $directory
 rm -R phpmyadmin
+############ blowfish_secret ############
 # secret_password="wDdAIJ*@#AU@#(J#fh@(D)_@(#@(JFPK"
 # sudo sed -i "s/\$cfg\['blowfish_secret'\] = ''\;/\$cfg\['blowfish_secret'\] = '$secret_password'\;/" config.inc.php
-sudo service mysql stop
-sudo service mysql start
-sudo mysql_secure_installation
-# CREATE USER 'new_user'@'localhost' IDENTIFIED BY 'password';
-# GRANT ALL PRIVILEGES ON * . * TO 'new_user'@'localhost';
-# FLUSH PRIVILEGES;
+#########################################
 sudo service apache2 stop
 sudo service apache2 start
+######### apache2 start warning #########
+# sudo echo -e "# Fix: AH00076: Failed to enable APR_TCP_DEFER_ACCEPT\nAcceptFilter http none\nAcceptFilter https none\n" >> /etc/apache2/apache2.conf
+#########################################
 echo "phpMyAdmin was successfully installed. access: yourdomain/phpmyadmin"
